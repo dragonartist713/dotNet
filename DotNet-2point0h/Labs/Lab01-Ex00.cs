8c0e19fb411429a640286e751e6572eef9330281
dragonartist713 <pantherspawstudios@gmail.com>
2024-01-18 11:05:56 -0600

    nothing works and i hate it

---
+++ DotNet-2point0h/Labs/Lab01-Ex00.cs
@@ -0,0 +1,24 @@
+class Lab0123{
+    public static void main(String[] args){
+
+    string[] words = new string[3];
+
+    for(int i = 0; i < words.length; i++){
+        System.out.println(words[i].length());
+    };
+
+
+
+
+    int[] myArray = new int[1001];
+
+    for(int i = 0; i < myArray.length; i++){
+        if(i % 0){
+            System.out.println("Even!");
+        };
+        if(i % 1){
+            System.out.println("Odd!");
+        };
+    };
+    }
+}
