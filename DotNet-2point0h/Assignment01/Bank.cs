1fb3951fb757a861384468b7bfac574580eaedb6
dragonartist713 <pantherspawstudios@gmail.com>
2024-01-23 10:48:23 -0600

    started assignment 1

---
+++ DotNet-2point0h/Assignment01/Bank.cs
@@ -0,0 +1,6 @@
+class Bank 
+{
+        Name: string;
+        AccountList: BankAccount[];
+        NextOpenIndex: int; //tracks next available index in acount list array
+}
---
+++ DotNet-2point0h/Assignment01/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace Assignment01
+{
+    class Bank {
+        //==============Attributes===============
+        //name: string
+        public string Name {get; set;}
+        //AccountList: BankAccount[]
+        BankAccount[] AccountList
+
+        //nextopenindex: int
+        public int NextOpenIndex //tracks next available index in account list array
+
+        //==============Methods==================
+
+        //bank(bankName: string, maxNumAccounts: int)
+        public Bank(string bankName, int maxNumAccounts){
+            this.Name = bankName
+            this.AccountList.length = maxNumAccounts
+        }
+
+        //bank(bankName: string, bankAccounts: BankAccount[])
+        public Bank(string bankName, ){
+            
+        }
+        //AddAccount(account: BankAccount): bool
+        //Transfer(accTransferFrom: BankAccount, accTransferTo: CankAccount, amount: double): bool
+        //AccountEcists(accountNumber: string): bool
+        //Capacity(): int
+    }
+
+    class BankAccount{
+        //==============Attributes===============
+        // MaxNegativeAmount: decimal = -50 {get; private set}
+        // HoldingBank: Bank
+        // AccountNumber: string
+        // Balance: decimal
+        //==============Methods==================
+        //BankAccount(holdingBank: Bank, accountNum: string)
+        public BankAccount(){
+            throw new InvalidOperationException("Cannot create account: The bank has no room for new accounts.")
+        }
+        //BankAccount(holdingBank: Bank, accountNum: string, accountBalance: decimal)
+        //Deposit(amount: decimal): void
+        //Withdraw(amount: decimal): decimal
+        //CheckBalance(): decimal
+        //GetMaxNegativeAmount(): decimal
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("=================== PROMPT 1 BEGIN ===================");
+
+            /* TODO: Write your code here. */
+
+            Console.WriteLine("=================== PROMPT 1 END ===================\n");
+
+
+            Console.WriteLine("=================== PROMPT 2 BEGIN ===================");
+
+            /* TODO: Write your code here. */
+
+            Console.WriteLine("=================== PROMPT 2 END ===================\n");
+
+
+            Console.WriteLine("=================== PROMPT 3 BEGIN ===================");
+
+            /* TODO: Write your code here. */
+
+            Console.WriteLine("=================== PROMPT 3 END ===================\n");
+
+
+            Console.WriteLine("=================== PROMPT 4 BEGIN ===================");
+
+            /* TODO: Write your code here. */
+
+            Console.WriteLine("=================== PROMPT 4 END ===================\n");
+
+
+            Console.WriteLine("=================== PROMPT 5 BEGIN ===================");
+
+            /* TODO: Write your code here. */
+
+            Console.WriteLine("=================== PROMPT 5 END ===================\n");
+
+
+            Console.WriteLine("=================== PROMPT 6 BEGIN ===================");
+
+            /* TODO: Write your code here. */
+
+            Console.WriteLine("=================== PROMPT 6 END ===================\n");
+
+
+            Console.WriteLine("=================== PROMPT 7 BEGIN ===================");
+
+            /* TODO: Write your code here. */
+
+            Console.WriteLine("=================== PROMPT 7 END ===================\n");
+
+
+            Console.WriteLine("=================== PROMPT 8 BEGIN ===================");
+
+            /* TODO: Write your code here. */
+
+            Console.WriteLine("=================== PROMPT 8 END ===================\n");
+
+
+            Console.WriteLine("=================== PROMPT 9 BEGIN ===================");
+
+            /* TODO: Write your code here. */
+
+            Console.WriteLine("=================== PROMPT 9 END ===================\n");
+        }
+    }
+}
