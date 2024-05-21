9fc88144614fc9c134ea520b37d56d153f8fd98c
Lindsay Hansen <pantherspawstudios@gmail.com>
2024-05-21 16:31:00 -0500

    pulled class repo

--- Database-Dotnet/assignment01/SchoolSystem/Migrations/20240508172013_InitialMigration.Designer.cs
+++ Database-Dotnet/assignment01/SchoolSystem/Migrations/20240508172013_InitialMigration.Designer.cs
@@ -2,17 +2,20 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SchoolSystem.Data;
 
 #nullable disable
 
-namespace SchoolSystem.Migrations
+namespace SchoolSystems.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240508172013_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.4");
@@ -82,7 +85,7 @@ namespace SchoolSystem.Migrations
                         .IsRequired()
                         .HasColumnType("TEXT");
 
-                    b.Property<DateTime?>("JoiningDate")
+                    b.Property<DateTime?>("JoinDate")
                         .HasColumnType("TEXT");
 
                     b.Property<string>("LastName")
@@ -104,7 +107,7 @@ namespace SchoolSystem.Migrations
                         .IsRequired()
                         .HasColumnType("TEXT");
 
-                    b.Property<DateTime?>("JoiningDate")
+                    b.Property<DateTime?>("JoinDate")
                         .HasColumnType("TEXT");
 
                     b.Property<string>("LastName")
