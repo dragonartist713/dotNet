f877b8485c3df0d024b0605a188916d28385ac3f
Lindsay Hansen <pantherspawstudios@gmail.com>
2024-05-31 23:10:07 -0500

    syncing home and github

--- Database-Dotnet/assignment01/SchoolSystem/Program.cs
+++ Database-Dotnet/assignment01/SchoolSystem/Program.cs
@@ -2,44 +2,59 @@
 using SchoolSystem.Data;
 using SchoolSystem.Models;
 using SchoolSystem.Services;
+using System;
+using System.Collections.Generic;
 
 ServiceProvider _serviceProvider;
-
 SeedingService _seedingService;
+
 BasicQueryService _basicQueryService;
 
-// This is the "container" we put the services in which
-// we want for dependency injection.
+
 var services = new ServiceCollection();
 
-// Add database to the service
 services.AddDbContext<ApplicationDbContext>();
-
-// Add seeding service to the service collection
 services.AddScoped<SeedingService>();
 services.AddScoped<BasicQueryService>();
 
-
-// Build the service collection
 _serviceProvider = services.BuildServiceProvider();
-
 _seedingService = _serviceProvider.GetRequiredService<SeedingService>();
+
 _basicQueryService = _serviceProvider.GetRequiredService<BasicQueryService>();
 
-// Seed the database
 _seedingService.SeedDatabase();
 
-// Call BasicQuerySerivce methods
-var instructorNames = _basicQueryService.GetAllInstructorNames();
+Console.WriteLine("============ NUMBER 1 - GetAllInstructorNames() ===================");
+// TODO: Call GetAllInstructorNames() and then print the results.
+foreach (string name in list){
+    _basicQueryService.GetAllInstructorNames();
+};
 
-// Print out everything in instructorNames
-// foreach(string instrName in instructorNames) {
-//     Console.WriteLine(instrName);
-// }
 
-// Console.WriteLine("=========================================");
-// var deptNames = _basicQueryService.GetDepartmentsWithCourses();
+Console.WriteLine("============ NUMBER 2 - GetStudentsInCourse() ===================");
+// TODO: Call GetStudentsInCourse() and then print the results for a specific course.
+// Console.WriteLine(_basicQueryService.GetStudentsInCourse("Calculus"));
 
-// foreach(string dept in deptNames) {
-//     Console.WriteLine(dept);
-// }
+Console.WriteLine("============ NUMBER 3 - GetDepartmentsWithMoreThanOneCourses() ===================");
+// TODO: Call GetDepartmentsWithMoreThanOneCourses() and then print the results.
+// Console.WriteLine(GetDepartmentsWithMoreThanOneCourses());
+
+Console.WriteLine("============ NUMBER 4 - GetDepartmentWithMostCourses() ===================");
+// TODO: Call GetDepartmentWithMostCourses() and then print the result.
+// Console.WriteLine(GetDepartmentWithMostCourses());
+
+Console.WriteLine("============ NUMBER 5 - GetStudentsEnrolledInMoreThanFiveCourses() ===================");
+// TODO: Call GetStudentsEnrolledInMoreThanFiveCourses() and then print the results.
+// Console.WriteLine(GetStudentsEnrolledInMoreThanFiveCourses());
+
+Console.WriteLine("============ NUMBER 6 - GetCoursesByInstructor() ===================");
+// TODO: Call GetCoursesByInstructor() with a specific instructor name and then print the results.
+// Console.WriteLine(GetCoursesByInstructor());
+
+Console.WriteLine("============ NUMBER 7 - GetStudentsWithNoCourses() ===================");
+// TODO: Call GetStudentsWithNoCourses() and then print the results.
+// Console.WriteLine(GetStudentsWithNoCourses());
+
+Console.WriteLine("============ NUMBER 8 - GetInstructorWithMostCourses() ===================");
+// TODO: Call GetInstructorWithMostCourses() and then print the result.
+// Console.WriteLine(GetInstructorWithMostCourses());
